171ac17d6000ceec4746e41cf00df4b5eed64792
Aiize-X <aiize.grow@gmail.com>
2022-09-28 19:37:15 -0700

    refix task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,56 +1,21 @@
 #include "main.h"
 
-int _strlen(char *s);
-int check_palindrome(char *s, int start, int end);
-
 /**
- * is_palindrome - returns 1 if a string is a palindrome and 0 if not.
- * @s: the string
+ * _strlen_recursion - Returns the length of a string.
+ * @s: The string to be measured.
  *
- * Return: returns 1 if a string is a palindrome and 0 if not.
+ * Return: The length of the string.
  */
-int is_palindrome(char *s)
+
+int _strlen_recursion(char *s)
 {
-	int len = _strlen(s);
+	int len = 0;
 
-	if (len == 0)
-		return (1);
+	if (*s)
+	{
+		len++;
+		len += _strlen_recursion(s + 1);
+	}
+	return (len);
 
-	return (check_palindrome(s, 0, len - 1));
-}
-
-/**
- * _strlen - gets the length of a string
- * @s: the string
- *
- * Return: the string length
- */
-int _strlen(char *s)
-{
-	if (*s == '\0')
-		return (0);
-
-	return (_strlen(++s) + 1);
-}
-
-/**
- * check_palindrome - checks if a substring is a palindrome
- * @s: the string
- * @start: the starting index of substring
- * @end: the stopping index of substring
- *
- * Return: 1 if substring is a palindrome, otherwise 0
- */
-int check_palindrome(char *s, int start, int end)
-{
-	if (start == end)
-		return (1);
-
-	if (s[start] != s[end])
-		return (0);
-
-	if (start < end + 1)
-		return (check_palindrome(s, start + 1, end - 1));
-
-	return (1);
 }
